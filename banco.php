61c5e8553f131694581aa69d0972bd9a50cd637a
MuriloIscuissati <muriloi.ifpr2024@gmail.com>
2025-02-06 08:26:02 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -1,262 +1,109 @@
-<?php 
+<?php
 
-$clientes = [];
-$contas   = [];
+print("Qual jogo você quer jogar? \n1) Mega-Sena \n2)Quina \n3)Lotomania \n4)Lotofácil: ");
 
+$tipo_jogo = readline("");
 
-function cadastrarCliente(&$clientes, string $nome, string $cpf, string $telefone): bool {
-    
-    //global $clientes; //Alternativa para acesso de variáveis fora do escopo da função
+while($tipo_jogo > 4 or $tipo_jogo <= 0){
 
-    if(strlen($telefone) != 10){
-
-        print("Telefone inválido. Seu número de telefone deve ter 10 digitos, com DDD incluso.\n");
-        return false;
-    }
-
-    if(!validar_cpf($cpf)){
-
-        print "CPF inválido.\n ";
-        return false;
-    }
-
-    $cliente = [
-        "nome" => $nome,
-        "cpf"  => $cpf, //11 digitos
-        "telefone" => $telefone //10 digitos
-    ];
-    
-    $clientes[] = $cliente;
-    return True;
-    
+    print("Opção Inválida: ");
+    $tipo_jogo = readline("");
 }
 
-function cadastrarConta(&$contas, $cpfCliente): string {
-    
-    $conta = [
-        "numeroConta" => uniqid(),
-        "cpfCliente" => $cpfCliente,
-        "saldo" => 0
-    ];
-    
-    $contas[] = $conta;
+switch ($tipo_jogo) {
+    case 1:
+        $dezenas_minimo = 6;        //mega-sena
+        $dezenas_maximo = 20;
+        $valor_minimo = 1;
+        $valor_maximo = 60;
 
-    return $conta['numeroConta'];
+        $valores[] = [5, 35, 140, 420, 1050, 2310, 4620, 8580, 15015, 25025, 40040, 61880, 92820, 135660, 193800];
+        break;
+    
+    case 2:
+        $dezenas_minimo = 5;        //quina
+        $dezenas_maximo = 15;
+        $valor_minimo = 1;
+        $valor_maximo = 80;
+
+        $valores[] = [2.50, 15, 52.50, 140, 315, 630, 1155, 1980, 3217.50, 5005, 7507.50];
+        break;
+
+    case 3:
+        $dezenas_minimo = 50;        //lotomania
+        $dezenas_maximo = 50;
+        $valor_minimo = 00;
+        $valor_maximo = 99;
+
+        $valores[] = [3];
+        break;
+    
+    case 4:
+        $dezenas_minimo = 15;        //lotofacil
+        $dezenas_maximo = 20;
+        $valor_minimo = 1;
+        $valor_maximo = 25;
+
+        $valores[] = [3, 48, 408, 2448, 11628, 46512];
+        break;
 }
 
-function depositar(&$contas, $numeroConta, $quantia){
+$numeroDezenas = readline("Quantas dezenas você quer jogar? ");
 
-    if($quantia < 0){
-        print("Não é possível depositar valores negativos.\n");
-        return;
-    }
+while($numeroDezenas < $dezenas_minimo or $numeroDezenas > $dezenas_maximo){
+
+    $numeroDezenas = readline("Número inválido. São aceitas apenas entre $dezenas_minimo e $dezenas_maximo dezenas. ");
+}
+
+function sorteador ($numeroDezenas, $minimo, $maximo){
     
-    foreach ($contas as &$conta){
+    $numerosSorteados = [];
+
+    for ($i = 0; $i < $numeroDezenas; $i++){
+
+        $numeroSorteado = random_int($minimo, $maximo);
         
-        if($conta['numeroConta'] == $numeroConta){
-            $conta['saldo'] += $quantia;
-
-            print "Depósito de R$$quantia realizado na conta $numeroConta.\n";
-            
-            return;
-        }
-    }
-
-    print "Conta não encontrada.\n";
-}
-
-function sacar(&$contas, $numeroConta, $quantia){
-
-    foreach ($contas as &$conta){
-
-        if($conta['numeroConta'] == $numeroConta){
-            
-            if($quantia > $conta['saldo']){
-                print("Sua conta não tem saldo suficiente para realizar a operação.\n");
-                break;               
-            }
-
-            if($quantia < 0){
-
-                print "Não é possível sacar valores negativos.\n";
-                break;
-            }
-
-            $conta['saldo'] -= $quantia;
-
-            print "Saque de R$$quantia realizado na conta $numeroConta.\n";
-
-            return;
+        if (in_array($numeroSorteado, $numerosSorteados)) {
+            $i--;
+            continue;
         }
         
-    }
-   
-        print "Conta $numeroConta não encontrada.\n";
+        $numerosSorteados[] = $numeroSorteado;
         
+    
+    
+    };
+    
+    sort($numerosSorteados);
+    print_r($numerosSorteados);
 }
 
-function consultarSaldo(&$contas, $numeroConta){
+function calculaPreco($valores, $numeroDezenas){
 
-    foreach ($contas as $conta){
-        if  ($conta['numeroConta'] == $numeroConta){
-            print "Saldo da conta {$numeroConta}: R$" . $conta['saldo'] . "\n";
-            return;
-        }
-
-    }
- 
-    print 'Conta não encontrada.\n';
+    $preco = $valores
 }
+/*function calculaPreco ($totalNumeros, $totalNumEscolhidos, $preco_base): float{
 
-function validar_cpf(string $cpf): bool {
-        
-    //soma1
-
-    if(strlen($cpf) != 11){
-        return false;
-    }
-
-    $soma = 0;
-
-    for ($i=0; $i < 9; $i++) { 
-        $soma += $cpf[$i] * (10 - $i);
-    }
-
+    $preco = fatorial($totalNumeros)/(fatorial($totalNumEscolhidos) * fatorial($totalNumeros - $totalNumEscolhidos));
     
-    $valor = (int) ($soma / 11);
-    $resto = $soma % 11;        
+    return $preco * $preco_base;
+};
 
-    if($resto < 2) {
-        $digito1 = 0;
-    } else if ($resto >= 2) {
-        $digito1 = (11 - $resto);
+function fatorial($numero){
+
+    $resultado =$numero;
+    if($numero <= 0){
+        return 1;
     }
-    
-    //soma2
+    for ($i = $numero; $i > 1; $i--){
 
-    $soma2 = 0;
-
-    for ($i=0; $i < 9; $i++) { 
-        $soma2 += ($cpf[$i] * (11 - $i));
+        $resultado = $resultado * ($i - 1);
     }
 
-    $soma2 += ($digito1 * 2);
-
-    $valor2 = (int) ($soma2 / 11);
-    $resto2 = $soma2 % 11;
-
-    // print "Valor2 $valor2 e resto2 $resto2";
-    // die;
-
-    
-    //Até aqui está OK!
+    return $resultado;
+};*/
 
 
-    if($resto2 < 2) {
-        $digito2 = 0;
-    } else if ($resto2 >= 2) {
-        $digito2 = (11 - $resto2);
-    }
 
-
-    //validação
-
-
-    if ($digito1 != $cpf[9]) {
-        $cpf = false;
-    } else {
-        if ($digito2 == $cpf[10]) {
-            return true;
-        } else {
-            return false;
-        } 
-    }
-
-    return false;
-}
-
-do{
-    
-    print "Bem-vindo ao banco Confiança Zero, onde você pode ter certeza que seus dados serão roubados!\n";
-    sleep(2);
-
-    print "O que gostaria de fazer?\n";
-    
-    print "1)Cadastrar \n2)Criar conta (necessário ter um cadastro) \n3)Depositar \n4)Sacar \n5)Consultar saldo \n6)Encerrar operações: ";
-    $resposta = readline("");
-
-    while($resposta < 1 or $resposta > 6){
-        
-        $resposta = readline("Resposta inválida. ");
-    }
-
-    switch ($resposta) {
-       
-        case '1':
-            
-            $nome = readline('Informe seu nome: ');
-            $cpf = readline((string)'Informe seu CPF: ');                           //cadastrar cliente
-            $telefone = readline('Informe seu telefone: ');
-
-            if(cadastrarCliente($clientes, $nome, $cpf, $telefone)){
-               
-                print 'Cadastro realizado!\n';
-            }
-            else{
-
-                print "Não foi possível realizar o cadastro.\n ";
-            }
-
-            break;
-        
-        case '2':
-            
-            $nome = readline('Informe o nome que você cadastrou: ');
-
-            foreach($clientes as $cliente){
-                
-                if($cliente['nome'] == $nome){
-
-                    $numeroConta = cadastrarConta($contas, $cliente['cpf']);        //cadastrar conta bancária
-                    
-                    print "Conta cadastrada! O número da sua conta é $numeroConta. Sugiro que anote-o agora!\n";
-                    break;
-                }
-                else{
-
-                    print  'Nome não cadastrado.';
-                }
-            }
-            break;
-            
-            case '3':
-
-                $numeroConta = readline("Informe o número da sua conta: ");
-                $quantia = readline("Informe quanto deseja depositar: ");           //depositar
-
-                depositar($contas, $numeroConta, $quantia);
-
-                break;
-            
-            case '4':
-                
-                $numeroConta = readline("Informe o número da sua conta: ");
-                $quantia = readline("Informe quanto deseja sacar: ");               //sacar
-
-                sacar($contas, $numeroConta, $quantia);
-
-                break;
-
-            case '5':
-
-                $numeroConta = readline("Informe o número da sua conta: ");        //constultar saldo
-
-                consultarSaldo($contas, $numeroConta);
-                break;
-            
-            sleep(2);
-     
-}
-}while($resposta != 6);
-
+sorteador($numeroDezenas, $valor_minimo, $valor_maximo);
 print "\nObrigado pela desconfiança!";
