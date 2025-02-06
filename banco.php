ad82162da333493d4b60eb01cce1db7f749d5e7a
MuriloIscuissati <muriloi.ifpr2024@gmail.com>
2025-02-05 22:58:42 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -4,9 +4,21 @@ $clientes = [];
 $contas   = [];
 
 
-function cadastrarCliente(&$clientes, string $nome, string $cpf, string $telefone): void {
+function cadastrarCliente(&$clientes, string $nome, string $cpf, string $telefone): bool {
     
     //global $clientes; //Alternativa para acesso de variáveis fora do escopo da função
+
+    if(strlen($telefone) != 10){
+
+        print("Telefone inválido. Seu número de telefone deve ter 10 digitos, com DDD incluso.\n");
+        return false;
+    }
+
+    if(!validar_cpf($cpf)){
+
+        print "CPF inválido.\n ";
+        return false;
+    }
 
     $cliente = [
         "nome" => $nome,
@@ -15,6 +27,7 @@ function cadastrarCliente(&$clientes, string $nome, string $cpf, string $telefon
     ];
     
     $clientes[] = $cliente;
+    return True;
     
 }
 
@@ -34,23 +47,22 @@ function cadastrarConta(&$contas, $cpfCliente): string {
 function depositar(&$contas, $numeroConta, $quantia){
 
     if($quantia < 0){
-        print("Não é possível depositar valores negativos");
+        print("Não é possível depositar valores negativos.\n");
         return;
     }
+    
     foreach ($contas as &$conta){
         
-        if($conta['$numeroConta' == $numeroConta]){
+        if($conta['numeroConta'] == $numeroConta){
             $conta['saldo'] += $quantia;
 
-            print "Depósito de R$$quantia realizado na conta $numeroConta.";
+            print "Depósito de R$$quantia realizado na conta $numeroConta.\n";
             
-            break;
-        }
-
-        else{
-
+            return;
         }
     }
+
+    print "Conta não encontrada.\n";
 }
 
 function sacar(&$contas, $numeroConta, $quantia){
@@ -60,33 +72,191 @@ function sacar(&$contas, $numeroConta, $quantia){
         if($conta['numeroConta'] == $numeroConta){
             
             if($quantia > $conta['saldo']){
-                print("Sua conta não tem saldo suficiente para realizar a operação.");
+                print("Sua conta não tem saldo suficiente para realizar a operação.\n");
                 break;               
+            }
+
+            if($quantia < 0){
+
+                print "Não é possível sacar valores negativos.\n";
+                break;
             }
 
             $conta['saldo'] -= $quantia;
 
-            print "Saque de R$$quantia realizado na conta $numeroConta";
+            print "Saque de R$$quantia realizado na conta $numeroConta.\n";
 
-            break;
+            return;
         }
-
-        else{
-
-            print "Conta $numeroConta não encontrada";
-        }
+        
     }
+   
+        print "Conta $numeroConta não encontrada.\n";
+        
 }
 
 function consultarSaldo(&$contas, $numeroConta){
 
     foreach ($contas as $conta){
         if  ($conta['numeroConta'] == $numeroConta){
-            print "Saldo da conta {$numeroConta}: R$$conta";
+            print "Saldo da conta {$numeroConta}: R$" . $conta['saldo'] . "\n";
+            return;
         }
-    }
-}
-cadastrarCliente($clientes, "Jefferson", "06800044455", "(45)99999999999");
-$numeroConta = cadastrarConta($contas, "06800044455");
 
-print_r($contas);
+    }
+ 
+    print 'Conta não encontrada.\n';
+}
+
+function validar_cpf(string $cpf): bool {
+        
+    //soma1
+
+    if(strlen($cpf) != 11){
+        return false;
+    }
+
+    $soma = 0;
+
+    for ($i=0; $i < 9; $i++) { 
+        $soma += $cpf[$i] * (10 - $i);
+    }
+
+    
+    $valor = (int) ($soma / 11);
+    $resto = $soma % 11;        
+
+    if($resto < 2) {
+        $digito1 = 0;
+    } else if ($resto >= 2) {
+        $digito1 = (11 - $resto);
+    }
+    
+    //soma2
+
+    $soma2 = 0;
+
+    for ($i=0; $i < 9; $i++) { 
+        $soma2 += ($cpf[$i] * (11 - $i));
+    }
+
+    $soma2 += ($digito1 * 2);
+
+    $valor2 = (int) ($soma2 / 11);
+    $resto2 = $soma2 % 11;
+
+    // print "Valor2 $valor2 e resto2 $resto2";
+    // die;
+
+    
+    //Até aqui está OK!
+
+
+    if($resto2 < 2) {
+        $digito2 = 0;
+    } else if ($resto2 >= 2) {
+        $digito2 = (11 - $resto2);
+    }
+
+
+    //validação
+
+
+    if ($digito1 != $cpf[9]) {
+        $cpf = false;
+    } else {
+        if ($digito2 == $cpf[10]) {
+            return true;
+        } else {
+            return false;
+        } 
+    }
+
+    return false;
+}
+
+do{
+    
+    print "Bem-vindo ao banco Confiança Zero, onde você pode ter certeza que seus dados serão roubados!\n";
+    sleep(2);
+
+    print "O que gostaria de fazer?\n";
+    
+    print "1)Cadastrar \n2)Criar conta (necessário ter um cadastro) \n3)Depositar \n4)Sacar \n5)Consultar saldo \n6)Encerrar operações: ";
+    $resposta = readline("");
+
+    while($resposta < 1 or $resposta > 6){
+        
+        $resposta = readline("Resposta inválida. ");
+    }
+
+    switch ($resposta) {
+       
+        case '1':
+            
+            $nome = readline('Informe seu nome: ');
+            $cpf = readline((string)'Informe seu CPF: ');                           //cadastrar cliente
+            $telefone = readline('Informe seu telefone: ');
+
+            if(cadastrarCliente($clientes, $nome, $cpf, $telefone)){
+               
+                print 'Cadastro realizado!\n';
+            }
+            else{
+
+                print "Não foi possível realizar o cadastro.\n ";
+            }
+
+            break;
+        
+        case '2':
+            
+            $nome = readline('Informe o nome que você cadastrou: ');
+
+            foreach($clientes as $cliente){
+                
+                if($cliente['nome'] == $nome){
+
+                    $numeroConta = cadastrarConta($contas, $cliente['cpf']);        //cadastrar conta bancária
+                    
+                    print "Conta cadastrada! O número da sua conta é $numeroConta. Sugiro que anote-o agora!\n";
+                    break;
+                }
+                else{
+
+                    print  'Nome não cadastrado.';
+                }
+            }
+            break;
+            
+            case '3':
+
+                $numeroConta = readline("Informe o número da sua conta: ");
+                $quantia = readline("Informe quanto deseja depositar: ");           //depositar
+
+                depositar($contas, $numeroConta, $quantia);
+
+                break;
+            
+            case '4':
+                
+                $numeroConta = readline("Informe o número da sua conta: ");
+                $quantia = readline("Informe quanto deseja sacar: ");               //sacar
+
+                sacar($contas, $numeroConta, $quantia);
+
+                break;
+
+            case '5':
+
+                $numeroConta = readline("Informe o número da sua conta: ");        //constultar saldo
+
+                consultarSaldo($contas, $numeroConta);
+                break;
+            
+            sleep(2);
+     
+}
+}while($resposta != 6);
+
+print "\nObrigado pela desconfiança!";
