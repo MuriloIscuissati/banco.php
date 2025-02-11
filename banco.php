416dec2a5dec2920dc71cb31ea151ddb26f61229
MuriloIscuissati <muriloi.ifpr2024@gmail.com>
2025-02-11 11:52:02 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -14,37 +14,41 @@ switch ($tipo_jogo) {
     case 1:
         $dezenas_minimo = 6;        //mega-sena
         $dezenas_maximo = 20;
-        $valor_minimo = 1;
-        $valor_maximo = 60;
+        $numero_minimo = 1;
+        $numero_maximo = 60;
+        $preco_base = 5;
 
-        $valores[] = [5, 35, 140, 420, 1050, 2310, 4620, 8580, 15015, 25025, 40040, 61880, 92820, 135660, 193800];
+        //$valores[] = [5, 35, 140, 420, 1050, 2310, 4620, 8580, 15015, 25025, 40040, 61880, 92820, 135660, 193800];
         break;
     
     case 2:
         $dezenas_minimo = 5;        //quina
         $dezenas_maximo = 15;
-        $valor_minimo = 1;
-        $valor_maximo = 80;
+        $numero_minimo = 1;
+        $numero_maximo = 80;
+        $preco_base = 2.5;
 
-        $valores[] = [2.50, 15, 52.50, 140, 315, 630, 1155, 1980, 3217.50, 5005, 7507.50];
+        //$valores[] = [2.50, 15, 52.50, 140, 315, 630, 1155, 1980, 3217.50, 5005, 7507.50];
         break;
 
     case 3:
         $dezenas_minimo = 50;        //lotomania
         $dezenas_maximo = 50;
-        $valor_minimo = 00;
-        $valor_maximo = 99;
+        $numero_minimo = 00;
+        $numero_maximo = 99;
+        $preco_base = 3;
 
-        $valores[] = [3];
+        //$valores[] = [3];
         break;
     
     case 4:
         $dezenas_minimo = 15;        //lotofacil
         $dezenas_maximo = 20;
-        $valor_minimo = 1;
-        $valor_maximo = 25;
+        $numero_minimo = 1;
+        $numero_maximo = 25;
+        $preco_base = 3;
 
-        $valores[] = [3, 48, 408, 2448, 11628, 46512];
+        //$valores[] = [3, 48, 408, 2448, 11628, 46512];
         break;
 }
 
@@ -77,33 +81,47 @@ function sorteador ($numeroDezenas, $minimo, $maximo){
     sort($numerosSorteados);
     print_r($numerosSorteados);
 }
-
-function calculaPreco($valores, $numeroDezenas){
-
-    $preco = $valores
+/**
+ * Calcula o fatorial de um número
+ */
+function fatorial($n) {
+    if ($n <= 1) return 1;
+    return $n * fatorial($n - 1);
 }
-/*function calculaPreco ($totalNumeros, $totalNumEscolhidos, $preco_base): float{
 
-    $preco = fatorial($totalNumeros)/(fatorial($totalNumEscolhidos) * fatorial($totalNumeros - $totalNumEscolhidos));
+/**
+ * Calcula a combinação C(n,r)
+ */
+function combinacao($n, $r) {
+    return fatorial($n) / (fatorial($r) * fatorial($n - $r));
+}
+
+/**
+ * Calcula o preço da aposta da Mega-Sena
+ */
+function calcular_preco($numeroDezenas, $preco_base, $numero_minimo, $numero_maximo, $dezenas_minimo) {
     
-    return $preco * $preco_base;
-};
-
-function fatorial($numero){
-
-    $resultado =$numero;
-    if($numero <= 0){
-        return 1;
+    if ($numeroDezenas < $numero_minimo || $numeroDezenas > $numero_maximo) {
+        return [
+            'erro' => true,
+            'mensagem' => "Número de dezenas deve ser entre {$numero_minimo} e {$numero_maximo}",
+            'preco' => 0,
+            'combinacoes' => 0
+        ];
     }
-    for ($i = $numero; $i > 1; $i--){
+    
+    $combinacoes = combinacao($numeroDezenas, $dezenas_minimo);
+    $preco = $preco_base * $combinacoes;
+    
+    return [
+        'erro' => false,
+        'mensagem' => '',
+        'preco' => $preco,
+        'combinacoes' => $combinacoes
+    ];
+}
 
-        $resultado = $resultado * ($i - 1);
-    }
-
-    return $resultado;
-};*/
 
 
-
-sorteador($numeroDezenas, $valor_minimo, $valor_maximo);
-print "\nObrigado pela desconfiança!";
+sorteador($numeroDezenas, $numero_minimo, $numero_maximo);
+print("O valor das apostas é R$" . calcular_preco($numeroDezenas, $preco_base, $numero_minimo, $numero_maximo, $dezenas_minimo)['preco'] . "\n");
